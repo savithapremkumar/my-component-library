9019446217b4a8d296beaf1b4fdbbb4d7bf31c71
SAVITHA PREMKUMAR <savithapremkumar@gmail.com>
2024-10-18 09:22:46 +1300

    Changing name of types file in packaje.json | Editing tsconfig.json to remove declaration here | Changing rollup.config.json to adapt to outputting types declaration in  both cjs and esm folders without errors

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,19 +8,30 @@ import terser from "@rollup/plugin-terser";
 // @ts-ignore
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-import packageJson from "./package.json" assert { type: "json" };
-
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: packageJson.main,
+        dir: "dist/cjs",
         format: "cjs",
         sourcemap: true,
       },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ declaration: true, declarationDir: "dist/cjs/types" }),
+      postcss(),
+      terser(),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [
       {
-        file: packageJson.module,
+        dir: "dist/esm",
         format: "esm",
         sourcemap: true,
       },
@@ -29,14 +40,15 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript({ declaration: true, declarationDir: "dist/esm/types" }),
       postcss(),
       terser(),
     ],
   },
+
   {
-    input: "src/index.ts",
-    output: [{ file: "dist/types.d.ts", format: "esm" }],
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
     external: [/\.css$/],
   },
